d3010e9aa5e5bdef1e4e4106e420e58523252c13
patrickryan01 <patrick.ryan0826@outlook.com>
2026-03-02 12:04:13 -0500

    rebrand: service-policy.ts

--- projects/ziti-console-lib/src/lib/models/service-policy.ts
+++
@@ -1,9 +0,0 @@
-export class ServicePolicy {
-    name = '';
-    serviceRoles: any[] = [];
-    identityRoles: any[] = [];
-    postureCheckRoles: any[] = [];
-    semantic = 'AnyOf';
-    type = 'Bind';
-    tags: any = {};
-};
