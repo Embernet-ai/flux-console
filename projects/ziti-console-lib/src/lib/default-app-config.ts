e1501f7310d1fe3592b6ce5cddb9a3ebf2275a98
patrickryan01 <patrick.ryan0826@outlook.com>
2026-03-02 12:03:43 -0500

    rebrand: default-app-config.ts

--- projects/ziti-console-lib/src/lib/default-app-config.ts
+++
@@ -1,9 +0,0 @@
-export type DefaultAppConfig = {
-    tunnelerEnabledReadOnly: boolean;
-    isOpenZiti?: boolean;
-};
-
-export const DEFAULT_APP_CONFIG_PROP = {
-    tunnelerEnabledReadOnly: true,
-    isOpenZiti: true
-};
